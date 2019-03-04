3814e05359e762939d9a3c1fc7634d7c4c82d332
shobu <shobu13@hotmail.fr>
2019-03-04 21:00:34 +0100

    First

---
+++ src/app/ui/base/base.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RendererComponent} from './renderer/renderer.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {FooterComponent} from './footer/footer.component';
+
+@NgModule({
+  declarations: [RendererComponent, NavbarComponent, FooterComponent],
+  exports: [RendererComponent,],
+  imports: [
+    CommonModule
+  ]
+})
+export class BaseModule {
+}
