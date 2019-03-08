94cc814be627f7e4cd89cac0b28b98714afd9797
Shobu <shobu13@hotmail.fr>
2019-03-08 17:32:17 +0100

    setup background image
    HAAAAAAAAAAAAAAAAAAAA

--- src/app/ui/base/base.module.ts
+++ src/app/ui/base/base.module.ts
@@ -5,7 +5,6 @@ import {NavbarComponent} from './navbar/navbar.component';
 import {FooterComponent} from './footer/footer.component';
 import {RouterModule} from '@angular/router';
 import {MastheadComponent} from './masthead/masthead.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [RendererComponent, NavbarComponent, FooterComponent, MastheadComponent],
